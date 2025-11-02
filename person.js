cbed96bbb3389ffe29d5f3ac9f1d5bf4069f8f99
prof-classmoji <prof.classmoji@gmail.com>
2025-11-02 13:37:24 -0500

    Update person.js

--- person.js
+++ person.js
@@ -17,12 +17,6 @@ class Person {
 
   haveBirthday() {
     this.age++;
-    return `Happy birthday! Now ${this.age} years old.`;
+    return `Happy birthday! Now 25 years old.`;
   }
 }
-
-// Example usage:
-const person1 = new Person("Alice", 25);
-console.log(person1.introduce()); // "Hi, I'm Alice and I'm 25 years old."
-console.log(person1.haveBirthday()); // "Happy birthday! Now 26 years old."
-
